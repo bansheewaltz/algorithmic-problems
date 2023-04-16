098f3450e8448b9f664c667b3a7759a1b3ab6581
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-04-16 20:15:50 +0700

    merge t3 branch changes

--- src/t3/t3.c
+++ src/t3/t3.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <errno.h>
 #include <math.h>
 #include <stdbool.h>
@@ -9,52 +10,202 @@
 #include "print_tree.h"
 #endif
 
-#define FILE_ACCESS_MODE "r"
-#define INPUT_FILE "2.txt"
+#define INPUT_FILE "input.txt"
+#define INPUT_READ_MODE "r"
 #define ALPHABET_SIZE 256
 #define BUFFER_SIZE 4096
 #define MAX_TREE_HEIGHT 50
 #define COUNTING_SORT_BASE 10
+// radix sort options:
+#define REVERSE true
+#define STRAIGHT false
 
 typedef unsigned char uchar;
-typedef unsigned int uint;
 
 typedef struct TreeNode TreeNode;
 struct TreeNode {
   uchar symbol;
-  uint freq;
+  int freq;
   TreeNode *left, *right;
 };
 
-int GetParentIdx(int i) {
-  return (i - 1) / 2;
+typedef struct Queue Queue;
+struct Queue {
+  int front, rear;
+  int capacity;
+  TreeNode **array;
+};
+
+void check_null_pointer(void *p) {
+  if (p == NULL) {
+    // fprintf(stderr, "Archiver error: %s\n", strerror(errno));
+    perror("Archiver error: ");
+    exit(1);
+  }
 }
 
-int GetLChildIdx(int i) {
-  return (2 * i + 1);
+TreeNode *create_tree_node(uchar symbol, int freq) {
+  TreeNode *temp = (TreeNode *)malloc(sizeof(TreeNode));
+  check_null_pointer(temp);
+
+  temp->left = temp->right = NULL;
+  temp->symbol = symbol;
+  temp->freq = freq;
+
+  return temp;
 }
 
-int GetRchildIdx(int i) {
-  return (2 * i + 2);
+Queue *create_queue(int capacity) {
+  Queue *queue = (Queue *)malloc(sizeof(Queue));
+  check_null_pointer(queue);
+
+  queue->front = queue->rear = -1;
+  queue->capacity = capacity;
+  queue->array = (TreeNode **)malloc(queue->capacity * sizeof(TreeNode *));
+
+  return queue;
 }
 
-bool NodeIsLeaf(TreeNode *node) {
+bool is_queue_size_one(Queue *queue) {
+  return queue->front == queue->rear && queue->front != -1;
+}
+
+bool is_queue_empty(Queue *queue) {
+  return queue->front == -1;
+}
+
+bool is_queue_full(Queue *queue) {
+  return queue->rear == queue->capacity - 1;
+}
+
+void enqueue(Queue *queue, TreeNode *item) {
+  assert(!is_queue_full(queue));
+
+  queue->array[++queue->rear] = item;
+  if (queue->front == -1) {
+    ++queue->front;
+  }
+}
+
+TreeNode *dequeue(Queue *queue) {
+  if (is_queue_empty(queue)) {
+    return NULL;
+  }
+
+  TreeNode *temp = queue->array[queue->front];
+  if (queue->front == queue->rear) {
+    queue->front = queue->rear = -1;
+  } else {
+    ++queue->front;
+  }
+
+  return temp;
+}
+
+void reverse_queue(Queue *queue) {
+  if (queue->front == -1) {
+    return;
+  }
+
+  TreeNode *item = dequeue(queue);
+  reverse_queue(queue);
+  enqueue(queue, item);
+}
+
+TreeNode *get_queue_front(Queue *queue) {
+  if (is_queue_empty(queue)) {
+    return NULL;
+  }
+
+  return queue->array[queue->front];
+}
+
+TreeNode *find_queue_min(Queue *firstQueue, Queue *secondQueue) {
+  if (is_queue_empty(firstQueue)) {
+    return dequeue(secondQueue);
+  }
+  if (is_queue_empty(secondQueue)) {
+    return dequeue(firstQueue);
+  }
+
+  if (get_queue_front(firstQueue)->freq < get_queue_front(secondQueue)->freq) {
+    return dequeue(firstQueue);
+  }
+  if (get_queue_front(firstQueue)->freq == get_queue_front(secondQueue)->freq) {
+    return dequeue(firstQueue);
+  }
+  return dequeue(secondQueue);
+}
+
+#ifdef DEBUG
+void display_queue(Queue *queue) {
+  if (queue->rear == -1) {
+    puts("Queue is empty");
+    return;
+  }
+
+  for (int i = queue->front; i <= queue->rear; i++) {
+    printf("%d ", queue->array[i]->symbol);
+  }
+
+  printf("\n");
+}
+#endif
+
+TreeNode *build_huffman_tree(uchar alph[], int freq[], int size) {
+  TreeNode *left, *right, *top;
+
+  Queue *firstQueue = create_queue(size);
+  Queue *secondQueue = create_queue(size);
+
+  for (int i = 0; i < size; ++i) {
+    enqueue(firstQueue, create_tree_node(alph[i], freq[i]));
+  }
+
+#ifdef DEBUG
+  puts("original queue:");
+  display_queue(firstQueue);
+#endif
+  reverse_queue(firstQueue);
+#ifdef DEBUG
+  puts("reversed queue:");
+  display_queue(firstQueue);
+#endif
+
+  while (!(is_queue_empty(firstQueue) && is_queue_size_one(secondQueue))) {
+    right = find_queue_min(firstQueue, secondQueue);
+    left = find_queue_min(firstQueue, secondQueue);
+
+    top = create_tree_node('$', left->freq + right->freq);
+    top->left = left;
+    top->right = right;
+    enqueue(secondQueue, top);
+  }
+
+  TreeNode *huffman_tree = dequeue(secondQueue);
+  free(firstQueue);
+  free(secondQueue);
+
+  return huffman_tree;
+}
+
+bool is_node_leaf(TreeNode *node) {
   return !(node->left) && !(node->right);
 }
 
-void DestroyTree(struct TreeNode *root) {
+void destroy_tree(struct TreeNode *root) {
   if (root == NULL) {
     return;
   }
 
-  DestroyTree(root->left);
-  DestroyTree(root->right);
+  destroy_tree(root->left);
+  destroy_tree(root->right);
 
   free(root);
   return;
 }
 
-void BuildDictionary(uint *dictionary, FILE *input) {
+void build_dictionary(int *dictionary, FILE *input) {
   uchar buffer[BUFFER_SIZE];
   int chars_read;
 
@@ -65,15 +216,8 @@ void BuildDictionary(uint *dictionary, FILE *input) {
   }
 }
 
-void NullPointerGuard(void *p) {
-  if (p == NULL) {
-    fprintf(stderr, "Archiver error: %s\n", strerror(errno));
-    exit(1);
-  }
-}
-
-uint GetAlphabetSize(uint *dictionary) {
-  uint alph_size = 0;
+int get_alphabet_size(int *dictionary) {
+  int alph_size = 0;
 
   for (int i = 0; i < ALPHABET_SIZE; ++i) {
     if (dictionary[i] != 0) {
@@ -84,27 +228,28 @@ uint GetAlphabetSize(uint *dictionary) {
   return alph_size;
 }
 
-void BuildAlphAndFreqArrays(uint *dictionary, uchar *symbols, uint *freq) {
-  uint i = 0;
-
-  for (int j = 0; j < ALPHABET_SIZE; ++j) {
+void build_alph_and_freq_arrays(int *dictionary, uchar *symbols, int *freq) {
+  for (int j = 0, i = 0; j < ALPHABET_SIZE; ++j) {
     if (dictionary[j] != 0) {
       symbols[i] = j;
       freq[i] = dictionary[j];
+      dictionary[j] = i;  // now the dictionary keeps indeces of the elements
+                          // in the new array
       ++i;
     }
   }
 }
 
-void PrintAlphFreqArraysLexicographically(uchar *symbols, uint *freq) {
+void print_alph_freq_arrays_lexicographically(uchar *symbols, int *freq,
+                                              FILE *output) {
   for (int i = 0; symbols[i]; ++i) {
-    printf("%d : %d\n", symbols[i], freq[i]);
+    fprintf(output, "%d : %d\n", symbols[i], freq[i]);
   }
 }
 
-TreeNode *MakeNewNode(char symbol, uint freq) {
+TreeNode *make_new_queue_node(char symbol, int freq) {
   TreeNode *temp = (TreeNode *)malloc(sizeof(TreeNode));
-  NullPointerGuard(temp);
+  check_null_pointer(temp);
 
   temp->left = temp->right = NULL;
   temp->symbol = symbol;
@@ -113,49 +258,176 @@ TreeNode *MakeNewNode(char symbol, uint freq) {
   return temp;
 }
 
-void PrintHCodeArray(int arr[], int n, FILE *output) {
-  for (int i = 0; i < n; ++i) {
-    fprintf(output, "%d", arr[i]);
+void counting_sort_paired(uchar alph[], int freq[], int size, int place,
+                          bool reverse) {
+  assert(size > 0);
+  int max_digit = COUNTING_SORT_BASE - 1;
+  int count[COUNTING_SORT_BASE] = {0};
+  int freq_output[size];
+  int alph_output[size];
+
+  // Calculate count of elements
+  for (int i = 0; i < size; ++i) {
+    count[(freq[i] / place) % 10]++;
+  }
+  // Calculate cumulative count
+  if (reverse) {
+    for (int i = max_digit; i > 0; --i) {
+      count[i - 1] += count[i];
+    }
+  } else {
+    for (int i = 1; i < max_digit + 1; ++i) {
+      count[i] += count[i - 1];
+    }
+  }
+  // Place the elements in sorted order
+  for (int i = size - 1; i >= 0; --i) {
+    alph_output[--count[(freq[i] / place) % 10]] = alph[i];
+    freq_output[count[(freq[i] / place) % 10]] = freq[i];
+  }
+  // Copy the sorted elements into original freq
+  for (int i = 0; i < size; ++i) {
+    alph[i] = alph_output[i];
+    freq[i] = freq_output[i];
+  }
+}
+
+int get_max_int(int array[], int n) {
+  int max = array[0];
+
+  for (int i = 1; i < n; ++i) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+
+  return max;
+}
+
+void radix_sort_paired(uchar alph[], int freq[], int size, bool reverse) {
+  int max = get_max_int(freq, size);
+
+  for (int place = 1; max / place > 0; place *= 10) {
+    counting_sort_paired(alph, freq, size, place, reverse);
+  }
+}
+
+#ifdef DEBUG
+void print_sorted_ascii_array(uchar alph[], int alph_size) {
+  puts("sorted array of symbols:");
+  for (int i = 0; i < alph_size; ++i) {
+    printf("%3d ", alph[i]);
+  }
+  putchar('\n');
+}
+#endif
+
+#ifdef DEBUG
+void print_sorted_freq_array(int freq[], int alph_size) {
+  for (int i = 0; i < alph_size; ++i) {
+    printf("%3d ", freq[i]);
+  }
+  putchar('\n');
+}
+#endif
+
+int *array_int_duplicate_with_shift(int src[], int len, int shift) {
+  int array_size = len + shift;
+  int *dst = (int *)malloc(sizeof(int) * array_size);
+
+  for (int i = shift; i < array_size; ++i) {
+    dst[i] = src[i - shift];
+  }
+
+  return dst;
+}
+
+int *array_int_duplicate(int src[], int len) {
+  int *dst = (int *)malloc(sizeof(int) * len);
+
+  for (int i = 0; i < len; ++i) {
+    dst[i] = src[i];
+  }
+
+  return dst;
+}
+
+uchar *array_char_duplicate(uchar src[], int len) {
+  uchar *dst = (uchar *)malloc(sizeof(uchar) * len);
+
+  for (int i = 0; i < len; ++i) {
+    dst[i] = src[i];
+  }
+
+  return dst;
+}
+
+int *copy_code_array(int src[], int code_len) {
+  int *dst = array_int_duplicate_with_shift(src, code_len, 1);
+
+  dst[0] = code_len;
+
+  return dst;
+}
+
+void scan_codes(TreeNode *root, int code[], int depth, int *codes[],
+                int dictionary[]) {
+  if (root->left) {
+    code[depth] = 0;
+    scan_codes(root->left, code, depth + 1, codes, dictionary);
+  }
+  if (root->right) {
+    code[depth] = 1;
+    scan_codes(root->right, code, depth + 1, codes, dictionary);
+  }
+
+  if (is_node_leaf(root)) {
+    codes[dictionary[root->symbol]] = copy_code_array(code, depth);
+  }
+}
+
+int **build_codes_array(TreeNode *tree, int dictionary[], int alph_size) {
+  int **codes = (int **)malloc(alph_size * sizeof(int *));
+  for (int i = 0; i < alph_size; ++i) {
+    codes[i] = NULL;
+  }
+
+  int code[MAX_TREE_HEIGHT];
+  int depth = 0;
+
+  scan_codes(tree, code, depth, codes, dictionary);
+
+  return codes;
+}
+
+int get_code_len(int *code) {
+  assert(code != NULL);
+  return code[0];
+}
+
+void print_code(uchar symbol, int code[], int code_len, FILE *output) {
+  fprintf(output, "%d : ", symbol);
+  for (int i = 1; i <= code_len; ++i) {
+    fprintf(output, "%d", code[i]);
   }
   fprintf(output, "\n");
 }
 
-void SearchSymbolOnTree(TreeNode *root, int arr[], int top, uint goal,
-                        uint *len, FILE *out) {
-  if (root->left) {
-    arr[top] = 0;
-    SearchSymbolOnTree(root->left, arr, top + 1, goal, len, out);
-  }
-  if (root->right) {
-    arr[top] = 1;
-    SearchSymbolOnTree(root->right, arr, top + 1, goal, len, out);
-  }
-  if (NodeIsLeaf(root) && root->symbol == goal) {
-    fprintf(out, "%d : ", root->symbol);
-    PrintHCodeArray(arr, top, out);
-    *len = top;
+void print_codes_lexicographically(uchar alph[], int *codes[], int alph_size,
+                                   FILE *output) {
+  for (int i = 0; i < alph_size; ++i) {
+    print_code(alph[i], codes[i], get_code_len(codes[i]), output);
   }
 }
 
-void PrintHCodesLexicographically(TreeNode *root, uchar alph[], uint lens[],
-                                  uint size, FILE *out) {
-  int arr[MAX_TREE_HEIGHT], top = 0;
-
-  for (uint i = 0; i < size; ++i) {
-    uint len = 0;
-    SearchSymbolOnTree(root, arr, top, alph[i], &len, out);
-    lens[i] = len;
-  }
-}
-
-void PrintAdditionalInfo(uint freq[], uint len[], int size, FILE *output) {
-  uint file_size = 0;
-  uint code_length = 0;
+void print_coding_stats(int freq[], int *codes[], int size, FILE *output) {
+  int file_size = 0;
+  int code_length = 0;
   double information_capacity = 0;
 
   for (int i = 0; i < size; ++i) {
     file_size += freq[i];
-    code_length += len[i] * freq[i];
+    code_length += get_code_len(codes[i]) * freq[i];
   }
   for (int i = 0; i < size; ++i) {
     information_capacity +=
@@ -170,99 +442,56 @@ void PrintAdditionalInfo(uint freq[], uint len[], int size, FILE *output) {
   fprintf(output, "Redundancy = %.6f\n", redundancy);
 }
 
-int GetMax(uint array[], uint n) {
-  uint max = array[0];
+void open_file_descriptors(FILE **input, FILE **output) {
+  *input = fopen(INPUT_FILE, INPUT_READ_MODE);
+  *output = fopen("output.txt", "w");
 
-  for (uint i = 1; i < n; ++i) {
-    if (array[i] > max) {
-      max = array[i];
-    }
-  }
-
-  return max;
-}
-
-void CountingSortPaired(uchar alph[], uint freq[], int size, int place) {
-  int max = COUNTING_SORT_BASE - 1;
-
-  // Initialize counting freq
-  int count[COUNTING_SORT_BASE];
-  for (int i = 0; i < max; ++i) {
-    count[i] = 0;
-  }
-  // Calculate count of elements
-  for (int i = 0; i < size; ++i) {
-    count[(freq[i] / place) % 10]++;
-  }
-  // Calculate cumulative count
-  for (int i = 1; i < 10; ++i) {
-    count[i] += count[i - 1];
-  }
-  // Place the elements in sorted order
-  int alph_output[size + 1];
-  int freq_output[size + 1];
-  for (int i = size - 1; i >= 0; i--) {
-    alph_output[count[(freq[i] / place) % 10] - 1] = alph[i];
-    freq_output[count[(freq[i] / place) % 10] - 1] = freq[i];
-    count[(freq[i] / place) % 10]--;
-  }
-  // Copy the sorted elements into original freq
-  for (int i = 0; i < size; ++i) {
-    alph[i] = alph_output[i];
-    freq[i] = freq_output[i];
-  }
-}
-
-void RadixSortPaired(uchar alph[], uint freq[], int size) {
-  int max = GetMax(freq, size);
-
-  for (int place = 1; max / place > 0; place *= 10) {
-    CountingSortPaired(alph, freq, size, place);
-  }
-}
-
-void PrintSortedAsciiArray(uchar alph[], int alph_size) {
-  for (int i = 0; i < alph_size; ++i) {
-    printf("%d ", alph[i]);
-  }
-  putchar('\n');
+  check_null_pointer(input);
+  check_null_pointer(output);
 }
 
 int main() {
   FILE *input = NULL;
-  uint dictionary[ALPHABET_SIZE] = {0};
+  FILE *output = NULL;
+  open_file_descriptors(&input, &output);
+  int dictionary[ALPHABET_SIZE] = {0};
+  int alph_size;
 
-  input = fopen(INPUT_FILE, FILE_ACCESS_MODE);
-  NullPointerGuard(input);
-  BuildDictionary(dictionary, input);
-  fclose(input);
-
-  uint alph_size = GetAlphabetSize(dictionary);
+  build_dictionary(dictionary, input);
+  alph_size = get_alphabet_size(dictionary);
+  assert(alph_size > 0);
   uchar *alph = (uchar *)malloc(alph_size * sizeof(uchar));
-  uint *freq = (uint *)malloc(alph_size * sizeof(uint));
-  BuildAlphAndFreqArrays(dictionary, alph, freq);
+  int *freq = (int *)malloc(alph_size * sizeof(int));
+  build_alph_and_freq_arrays(dictionary, alph, freq);
 #ifdef DEBUG
-  PrintAlphFreqArraysLexicographically(alph, freq);
+  print_alph_freq_arrays_lexicographically(alph, freq, stdout);
 #endif
-  RadixSortPaired(alph, freq, alph_size);
+  uchar *alph_sorted = array_char_duplicate(alph, alph_size);
+  int *freq_sorted = array_int_duplicate(freq, alph_size);
+  radix_sort_paired(alph_sorted, freq_sorted, alph_size, REVERSE);
 #ifdef DEBUG
-  PrintSortedAsciiArray(alph, alph_size);
+  print_sorted_ascii_array(alph_sorted, alph_size);
+  print_sorted_freq_array(freq_sorted, alph_size);
+#endif
+  TreeNode *tree = build_huffman_tree(alph_sorted, freq_sorted, alph_size);
+  int **codes = build_codes_array(tree, dictionary, alph_size);
+  print_codes_lexicographically(alph, codes, alph_size, output);
+  print_coding_stats(freq, codes, alph_size, output);
+#ifdef DEBUG
+  print_codes_lexicographically(alph, codes, alph_size, stdout);
 #endif
 
-  //   FILE *output = NULL;
-  //   output = fopen("output.txt", "w");
-  //   NullPointerGuard(output);
-  //   uint *lens = (uint *)malloc(alph_size * sizeof(uint));
-  //   TreeNode *tree = HuffmanCodes(alph, freq, lens, alph_size, output);
-  // #ifdef DEBUG
-  //   DisplayTree(tree);
-  // #endif
-  //   PrintAdditionalInfo(freq, lens, alph_size, output);
-  //   fclose(output);
+  destroy_tree(tree);
+  for (int i = 0; i < alph_size; ++i) {
+    free(codes[i]);
+  }
+  free(codes);
+  free(alph);
+  free(freq);
+  free(alph_sorted);
+  free(freq_sorted);
+  fclose(input);
+  fclose(output);
 
-  //   DestroyTree(tree);
-  //   free(alph);
-  //   free(freq);
-  //   free(lens);
   return EXIT_SUCCESS;
 }
