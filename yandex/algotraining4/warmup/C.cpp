e03001a1f08c9cc870ca62840796957b611fc498
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-11-10 17:34:39 +0700

    ya:algo4:w:C: remove ternary operators

--- yandex/algotraining4/warmup/C.cpp
+++ yandex/algotraining4/warmup/C.cpp
@@ -23,22 +23,20 @@ void solve() {
   double ra = sqrt(xa * xa + ya * ya);
   double rb = sqrt(xb * xb + yb * yb);
 
-  double xsm, xbg, ysm, ybg, rsm, rbg;
-
-  ra < rb ? rsm = ra : rsm = rb;
-  ra > rb ? rbg = ra : rbg = rb;
+  double rsm, rbg;
+  rsm = min(ra, rb);
+  rbg = max(ra, rb);
 
   double anga = atan2(xa, ya);
   double angb = atan2(xb, yb);
-
   double angab_1 = abs(anga - angb);
   double angab_2 = 2 * M_PI - angab_1;
-  double angab_sm = angab_1 < angab_2 ? angab_1 : angab_2;
+  double angab_sm = min(angab_1, angab_2);
   double arc = rsm * angab_sm;
 
   double arcpath = arc + rbg - rsm;
   double cenpath = ra + rb;
-  double shortest = arcpath < cenpath ? arcpath : cenpath;
+  double shortest = min(arcpath, cenpath);
 
   cout << fixed << setprecision(12) << shortest;
 }
