16dafaf93821f4e727f6ef76237925a4f05bf8d3
Anton Grevtsov <grevtsov.apn@gmail.com>
2024-04-08 15:32:33 +0700

    refactor: tester

--- tester.cpp
+++ tester.cpp
@@ -15,14 +15,14 @@ using std::string;
 string get_testspath(int argc, char *argv[]) {
   if (argc == 3) 
     return argv[2];
-  string binpath = argv[1];
-  int binname_beg_pos = binpath.find_last_of("/") + 1;
-  int binname_end_pos = binpath.find_last_of(".");
-  int hyphen_pos = binpath.find_last_of("-");
+  string bin_path = argv[1];
+  int binname_beg_pos = bin_path.find_last_of("/") + 1;
+  int binname_end_pos = bin_path.find_last_of(".");
+  int hyphen_pos = bin_path.find_last_of("-");
   if (hyphen_pos > binname_beg_pos) 
     binname_end_pos = hyphen_pos;
   int binname_len = binname_end_pos - binname_beg_pos;
-  return binpath.substr(binname_beg_pos, binname_len) + ".test";
+  return bin_path.substr(binname_beg_pos, binname_len) + ".test";
 }
 
 string rstripws(string line) {
@@ -40,43 +40,45 @@ string add_indentation(string s) {
   return fmt;
 }
 
-const string test_in = "test_in.txt";
-const string test_out = "test_out.txt";
+const string tests_in = "in.txt";
+const string tests_out = "out.txt";
 const string key_input = "---";
 const string key_solution = "===";
 
 int main(int argc, char *argv[]) {
   if (argc == 1) return 1;
 
-  string binpath = argv[1];
-  string testspath = get_testspath(argc, argv);
-  if (!freopen(testspath.c_str(), "r", stdin)) {
-    std::cerr << "there is no such file: " << testspath << std::endl;
+  string bin_path = argv[1];
+  string tests_path = get_testspath(argc, argv);
+  if (!freopen(tests_path.c_str(), "r", stdin)) {
+    std::cerr << "there is no such file: " << tests_path << std::endl;
     return 1;
   }
+  const string syscall = "./" + bin_path + " < " + tests_in + " > " + tests_out;
 
   int test_n = 0;
   int line_n = 0;
   int last_test_pointer = 1;
-  string syscall = "./" + binpath + " < " + test_in + " > " + test_out;
-
-  string line;
+  string line, line_next;
   std::stringstream buffer;
-  while (std::getline(std::cin, line)) {
+
+  std::getline(std::cin, line);
+  while (!std::cin.eof()) {
     line = rstripws(line);
     line_n++;
+    if (line != key_input && line != key_solution)
+      buffer << line << '\n';
     if (line == key_solution) {
       test_n++;
-      std::ofstream ofs{test_in};
+      std::ofstream ofs{tests_in};
       ofs << buffer.str();
       ofs.close();
       system(syscall.c_str());
       buffer.str("");
-      continue;
     }
-    if (line == key_input) {
-    check_res:
-      std::ifstream res_ifs{test_out};
+    std::getline(std::cin, line_next);
+    if (line == key_input || std::cin.eof()) {
+      std::ifstream res_ifs{tests_out};
       std::stringstream ans;
       ans << res_ifs.rdbuf();
       using std::cout;
@@ -84,15 +86,13 @@ int main(int argc, char *argv[]) {
         cout << test_n << ". OK" << std::endl;
       else {
         cout << test_n << ". FAIL";
-        cout << "\t" << testspath << ":" << last_test_pointer << std::endl;
+        cout << "\t" << tests_path << ":" << last_test_pointer << std::endl;
         cout << "=======\n";
         cout << add_indentation(ans.str());
       } 
       buffer.str("");
       last_test_pointer = line_n + 1;
-      continue;
     }
-    buffer << line << '\n';
+    line = line_next;
   }
-  if (buffer.tellp() != std::streampos(0)) goto check_res;
 }
